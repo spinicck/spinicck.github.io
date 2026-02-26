8db5cf805f431be965814d31ce3b5f6381db02ac
Kévin SPINICCI <kevin.spinicci@outlook.com>
2026-02-26 19:51:38 +0100

    switch to WebAnim for better handling of in/out animation

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ var currentOpenedModal = null
 /**
  * Observer to animate element appearances on screen
  */
-const showOnScrollObserver = new IntersectionObserver( (entries) => {
-    entries.forEach( (entry) => {
+const showOnScrollObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.toggle("show")
         } else {
@@ -22,7 +22,7 @@ const showOnScrollObserver = new IntersectionObserver( (entries) => {
 
 /* Get elements with scroll animation and add observer. */
 const showOnScrollElements = document.querySelectorAll(".show-on-scroll.hidden")
-showOnScrollElements.forEach( (el) => showOnScrollObserver.observe(el) );
+showOnScrollElements.forEach((el) => showOnScrollObserver.observe(el));
 
 /**
  * Show/hide the navbar when the navbar toggle button is pressed.
@@ -33,7 +33,7 @@ function toggleNavbar() {
     var navbar = document.getElementById("nav-bar");
     var navToggleIcon = document.getElementById("nav-toggle-icon");
     /* Only trigger toggle event if screen size is smaller than 800px */
-    if ( !matchMedia("(max-width: 800px)").matches )  {
+    if (!matchMedia("(max-width: 800px)").matches) {
         return;
     }
     if (navbar.classList.contains("compact")) {
@@ -47,7 +47,7 @@ function toggleNavbar() {
 /**
  * Function to switch between light and dark theme.
  */
-function toggleLightDarkMode(){
+function toggleLightDarkMode() {
     const hmtlTag = document.getElementsByTagName("html")[0];
     const navThemeIcon = document.getElementById("nav-theme-icon")
     if (hmtlTag.className === "light") {
@@ -59,59 +59,60 @@ function toggleLightDarkMode(){
     }
 }
 
-function openCard(e){
-    /* For now duplicate the code when browser cannot handle transition API.
-     * If method become more complex, create dedicated function.
-     */
+async function openCard(e) {
+    console.debug("Call openCard: ", e)
     currentOpenedCard = e
-    currentOpenedCard.style.viewTransitionName = "modal"
-    currentOpenedCard.style.filter = "blur(5px)"
-    /* Create modal for page transition */
-    let modal = currentOpenedCard.getElementsByTagName("modal-content")[0].cloneNode(true)
-    modal.onclick = function(){
-        closeCard()
-    }
-    currentOpenedModal = modal
-
-    /* Fallback for browsers that don't support this API */
-    if (!document.startViewTransition) {
-        document.body.appendChild(modal)
-        modal.style.display = "flex"
-        return;
-    }
-
-    /* With a View Transition */
-    document.startViewTransition(() => {
-        document.body.appendChild(modal)
-        modal.style.display = "flex"
-        currentOpenedCard.style.viewTransitionName = ""
-        currentOpenedCard.style.filter = ""
-    });
+    /* Compute center of viewport */
+    let centerX = window.innerWidth / 2
+    let centerY = window.innerHeight / 2
+    /* Compute center of current element */
+    let bbox = currentOpenedCard.getBoundingClientRect()
+    let coordX = bbox.x + (bbox.width / 2)
+    let coordY = bbox.y + (bbox.height / 2)
+    /* Compute translation vector */
+    let transVectX = centerX - coordX
+    let transVectY = centerY - coordY
+    let animation = await currentOpenedCard.animate(
+        { transform: `translate(${transVectX}px, ${transVectY}px)` },
+        { duration: 400, fill: "forwards" }
+    ).finished
+    animation.commitStyles()
+    animation.cancel()
+    currentOpenedModal = currentOpenedCard.getElementsByTagName("modal-content")[0].cloneNode(true)
+    currentOpenedModal.style.display = "flex"
+    currentOpenedModal.onclick = closeModal
+    document.body.appendChild(currentOpenedModal)
+    animation = await currentOpenedModal.animate(
+        [
+            { opacity: 0, transform: "scale(0)" },
+            { opacity: 1, transform: "scale(1)" }
+        ],
+        { duration: 400, fill: "forwards" }
+    ).finished
+    animation.commitStyles()
+    animation.cancel()
 }
 
-function closeCard(){
-    /* For now duplicate the code when browser cannot handle transition API.
-     * If method become more complex, create dedicated function.
-     */
-    console.debug(`currentOpenedModal: ${currentOpenedModal}`)
-    console.debug(`currentOpenedCard: ${currentOpenedCard}`)
+async function closeModal() {
+    console.debug("Close Modal")
+    let animation = await currentOpenedModal.animate(
+        [
+            { opacity: 1, transform: "scale(1)" },
+            { opacity: 0, transform: "scale(0)" }
+        ],
+        { duration: 400, fill: "forwards" }
+    ).finished
+    animation.commitStyles()
+    animation.cancel()
+    currentOpenedModal.style.display = "none"
+    currentOpenedModal.remove()
+    currentOpenedModal = null
 
-    /* Fallback for browsers that don't support this API */
-    if (!document.startViewTransition) {
-        currentOpenedModal.style.display = "none"
-        currentOpenedModal.remove()
-        currentOpenedCard.style.visibility = "visible"
-        currentOpenedModal = null
-        currentOpenedCard = null
-        return;
-    }
-
-    /* With a View Transition */
-    document.startViewTransition(() => {
-        currentOpenedModal.style.display = "none"
-        currentOpenedModal.remove()
-        currentOpenedCard.style.visibility = "visible"
-        currentOpenedModal = null
-        currentOpenedCard = null
-    });
+    animation = await currentOpenedCard.animate(
+        { transform: "translate(0)" },
+        { duration: 400, fill: "forwards" }
+    ).finished
+    animation.commitStyles()
+    animation.cancel()
+    currentOpenedCard = null
 }
